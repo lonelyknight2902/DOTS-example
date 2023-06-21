8b2d6db7f48c0b0041d727d4b0917fd7b7ded110
lonelyknight2902 <truongthinh2902@gmail.com>
2023-06-21 09:37:49 +0700

    Update user input

--- Assets/Scripts/GunSystem.cs
+++ Assets/Scripts/GunSystem.cs
@@ -20,9 +20,27 @@ public partial struct GunSystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-            foreach (var (localToWorld, gun) in SystemAPI.Query<RefRW<LocalToWorld>, RefRW<Gun>>())
+        // bool isPressedSpace;
+        // if (Input.GetKey(KeyCode.Space))
+        // {
+        //     isPressedSpace = true;
+        // }
+        // else
+        // {
+        //     isPressedSpace = false;
+        // }
+        var isPressedSpace = Input.GetKey(KeyCode.Space);
+        foreach (var (localToWorld, gun) in SystemAPI.Query<RefRW<LocalToWorld>, RefRW<Gun>>())
+        {
+            
+            // Debug.Log(gun.ValueRO.NextSpawnTime);
+            if (!isPressedSpace)
             {
-                if (gun.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime)
+                gun.ValueRW.NextSpawnTime = 0;
+            }
+            else
+            {
+                if (gun.ValueRO.NextSpawnTime <= 0)
                 {
                     var bulletInstance = state.EntityManager.Instantiate(gun.ValueRO.BulletPrefab);
                     state.EntityManager.SetComponentData(bulletInstance, new LocalTransform
@@ -36,9 +54,36 @@ public partial struct GunSystem : ISystem
                     {
                         velocity = localToWorld.ValueRO.Up * 20f,
                     });
-                    gun.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + gun.ValueRO.ShootRate;
+                    gun.ValueRW.NextSpawnTime = gun.ValueRO.ShootRate;
+                    Debug.Log(gun.ValueRO.NextSpawnTime);
                 }
-                
+                else
+                {
+                    gun.ValueRW.NextSpawnTime -= SystemAPI.Time.DeltaTime;
+                }
             }
+            
+                
+        }
+        // foreach (var (localToWorld, gun) in SystemAPI.Query<RefRW<LocalToWorld>, RefRW<Gun>>())
+        // {
+        //     if (gun.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime)
+        //     {
+        //         var bulletInstance = state.EntityManager.Instantiate(gun.ValueRO.BulletPrefab);
+        //         state.EntityManager.SetComponentData(bulletInstance, new LocalTransform
+        //         {
+        //             Position = SystemAPI.GetComponent<LocalToWorld>(gun.ValueRO.FirePoint).Position,
+        //             Rotation = SystemAPI.GetComponent<LocalTransform>(gun.ValueRO.BulletPrefab).Rotation,
+        //             Scale = SystemAPI.GetComponent<LocalTransform>(gun.ValueRO.BulletPrefab).Scale,
+        //         });
+        //     
+        //         state.EntityManager.SetComponentData(bulletInstance, new Bullet
+        //         {
+        //             velocity = localToWorld.ValueRO.Up * 20f,
+        //         });
+        //         gun.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + gun.ValueRO.ShootRate;
+        //     }
+        //     
+        // }
     }
 }
