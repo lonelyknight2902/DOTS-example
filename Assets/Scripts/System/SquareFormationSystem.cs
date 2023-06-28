8a4da0bac57f8962af5ddf9e689ec24183d89b92
lonelyknight2902 <truongthinh2902@gmail.com>
2023-06-28 10:17:34 +0700

    Update job for square formation spawner

--- Assets/Scripts/System/SquareFormationSystem.cs
+++ Assets/Scripts/System/SquareFormationSystem.cs
@@ -10,11 +10,24 @@ namespace System
     {
         public void OnUpdate(ref SystemState state)
         {
-            EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.Temp);
-            new SquareFormationJob
+            // EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.Temp);
+            // new SquareFormationJob
+            // {
+            //     ecb = ecb
+            // }.ScheduleParallel();
+            // ecb.Playback(state.EntityManager);
+            // ecb.Dispose();
+            
+            var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+            var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+            
+            var job = new SquareFormationJob
             {
-                ecb = ecb
-            }.ScheduleParallel();
+                ecb = ecb.AsParallelWriter()
+            };
+            
+            state.Dependency = job.Schedule(state.Dependency);
+            
         }
     }
 }
@@ -22,13 +35,14 @@ namespace System
 [WithAll(typeof(SquareFormation))]
 public partial struct SquareFormationJob : IJobEntity
 {
-    public EntityCommandBuffer ecb;
-    public void Execute(ref FormationSpawn formationSpawn)
+    public EntityCommandBuffer.ParallelWriter ecb;
+    public void Execute([ChunkIndexInQuery] int chunkIndex, ref FormationSpawn formationSpawn, Entity entity)
     {
-        SquareSpawn(3, formationSpawn.spawnPosition, formationSpawn.enemy);
+        SquareSpawn(chunkIndex, 3, formationSpawn.spawnPosition, formationSpawn.enemy);
+        ecb.DestroyEntity(chunkIndex, entity);
     }
     
-    private void SquareSpawn(int side, float3 spawnPosition, Entity enemy)
+    private void SquareSpawn(int index, int side, float3 spawnPosition, Entity enemy)
     {
         // float spawnbound = 2f;
         // float randomEne = randomEnemy.NextFloat(-1, 1);
@@ -38,7 +52,7 @@ public partial struct SquareFormationJob : IJobEntity
             {
                 for (int j = 0; j < side; j++)
                 {
-                    Entity newEntity = ecb.Instantiate(enemy);
+                    Entity newEntity = ecb.Instantiate(index, enemy);
                     // Entity newEntity;
                     // if (randomEne > 0)
                     // {
@@ -48,13 +62,13 @@ public partial struct SquareFormationJob : IJobEntity
                     // {
                     //     newEntity = state.EntityManager.Instantiate(spawner.ValueRO.redPrefab);
                     // }
-                    ecb.SetComponent(newEntity, LocalTransform.FromPosition(new float3(spawnPosition.x - j*4, spawnPosition.y, spawnPosition.z - i*4)));
+                    ecb.SetComponent(index, newEntity, LocalTransform.FromPosition(new float3(spawnPosition.x - j*4, spawnPosition.y, spawnPosition.z - i*4)));
                 }
             }
             else
             {
-                Entity newEntity1 = ecb.Instantiate(enemy);
-                Entity newEntity2 = ecb.Instantiate(enemy);
+                Entity newEntity1 = ecb.Instantiate(index, enemy);
+                Entity newEntity2 = ecb.Instantiate(index, enemy);
                 // Entity newEntity1;
                 // Entity newEntity2;
                 // if (randomEne > 0)
@@ -67,8 +81,8 @@ public partial struct SquareFormationJob : IJobEntity
                 //     newEntity1 = state.EntityManager.Instantiate(spawner.ValueRO.redPrefab);
                 //     newEntity2 = state.EntityManager.Instantiate(spawner.ValueRO.redPrefab);
                 // }
-                ecb.SetComponent(newEntity1, LocalTransform.FromPosition(new float3(spawnPosition.x, spawnPosition.y, spawnPosition.z - i*4)));
-                ecb.SetComponent(newEntity2, LocalTransform.FromPosition(new float3( spawnPosition.x - (side-1)*4, spawnPosition.y, spawnPosition.z - i*4)));
+                ecb.SetComponent(index, newEntity1, LocalTransform.FromPosition(new float3(spawnPosition.x, spawnPosition.y, spawnPosition.z - i*4)));
+                ecb.SetComponent(index, newEntity2, LocalTransform.FromPosition(new float3( spawnPosition.x - (side-1)*4, spawnPosition.y, spawnPosition.z - i*4)));
             }
         }
     }
