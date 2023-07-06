cfde2562b47b296a30e615692d55f448f6f39115
lonelyknight2902 <truongthinh2902@gmail.com>
2023-07-06 16:48:49 +0700

    Add restart button

---
+++ Assets/Scripts/UI/RestartGameButton.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class RestartGameButton : MonoBehaviour
+    {
+        private Button _button;
+        
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(RestartGame);
+        }
+
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveListener(RestartGame);
+        }
+
+        private void RestartGame()
+        {
+            Debug.Log("Restart game");
+            // World world = World.DefaultGameObjectInjectionWorld;
+            // var entityManager = world.EntityManager;
+            // entityManager.DestroyEntity(entityManager.UniversalQuery);
+            // SceneManager.LoadSceneAsync("Scenes/SampleScene");
+        }
+    }
+}
